7f9cc8c5da0264337dd193fc77abf0e1c464761c
Luke McGuire <luke.mcguire@gmail.com>
2025-02-26 09:57:33 -0800

    feat: add get books by author endpoint

--- LM/project-1/books.py
+++ LM/project-1/books.py
@@ -33,6 +33,15 @@ async def read_category_by_query(category: str):
     return books_to_return
 
 
+@app.get("/books/author/{book_author}")
+async def read_books_by_author(book_author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == book_author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.get("/books/{book_author}/")
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
@@ -55,6 +64,7 @@ async def update_book(updated_book: dict = Body(...)):
     for i in range(len(BOOKS)):
         if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
             BOOKS[i] = updated_book
+
 
 @app.delete("/books/delete_book/{book_title}")
 async def delete_book(book_title: str):
