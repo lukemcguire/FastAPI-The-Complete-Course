05c4436ed5da379650bc408254ffd4e3c405d8a9
Luke McGuire <luke.mcguire@gmail.com>
2025-02-27 10:28:23 -0800

    feat: add create_todo endpoint

--- LM/project-3/TodoApp/main.py
+++ LM/project-3/TodoApp/main.py
@@ -1,5 +1,7 @@
 from typing import Annotated
 
+from pydantic import BaseModel, Field
+
 import models
 from database import SessionLocal, engine
 from fastapi import Depends, FastAPI, HTTPException, Path, status
@@ -21,6 +23,11 @@ def get_db():
 
 db_depenency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title: str = Field(..., min_length=3, max_length=100)
+    description: str = Field(..., min_length=3, max_length=100)
+    priority: int = Field(..., ge=1, le=5)
+    complete: bool
 
 @app.get("/")
 async def read_all(db: db_depenency):
@@ -33,3 +40,9 @@ async def read_todo(todo_id: Annotated[int, Path(..., gt=0)], db: db_depenency):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(todo: TodoRequest, db: db_depenency):
+    todo_model = Todos(**todo.model_dump())
+    db.add(todo_model)
+    db.commit()
