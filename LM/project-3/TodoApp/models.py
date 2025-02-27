eab4b709e38e8447854ad3be17a3d657453de340
Luke McGuire <luke.mcguire@gmail.com>
2025-02-27 12:10:43 -0800

    feat: add create user functionality

--- LM/project-3/TodoApp/models.py
+++ LM/project-3/TodoApp/models.py
@@ -1,12 +1,26 @@
 from database import Base
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String, default="user")
 
 
 class Todos(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=False)
-    description = Column(String, index=False)
-    priority = Column(Integer, index=False)
-    complete = Column(Boolean, default=False, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
--- LM/project-3/TodoApp/routers/auth.py
+++ LM/project-3/TodoApp/routers/auth.py
@@ -1,8 +1,46 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from database import SessionLocal
+from fastapi import APIRouter, Depends, status
+from models import Users
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_depenency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_depenency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        role=create_user_request.role,
+    )
+
+    db.add(create_user_model)
+    db.commit()
