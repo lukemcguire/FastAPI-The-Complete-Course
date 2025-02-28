4635c153c3cedafadd0ce3c013d85d298ae7d1ec
Luke McGuire <luke.mcguire@gmail.com>
2025-02-28 11:36:59 -0800

    update: revise change phone number endpoint

--- LM/project-3/TodoApp/routers/users.py
+++ LM/project-3/TodoApp/routers/users.py
@@ -31,11 +31,6 @@ class UserVerification(BaseModel):
     new_password: str = Field(..., min_length=8, max_length=100)
 
 
-class UserPhoneVerification(BaseModel):
-    password: str
-    new_phone_number: str = Field(..., min_length=10, max_length=15)
-
-
 @router.get("/get_user", status_code=status.HTTP_200_OK)
 async def read_user_details(db: db_depenency, user: user_dependency):
     if user is None:
@@ -67,12 +62,10 @@ async def change_password(db: db_depenency, user: user_dependency, user_verifica
 
 
 @router.put("/change_phone_number", status_code=status.HTTP_204_NO_CONTENT)
-async def change_phone_number(db: db_depenency, user: user_dependency, user_verification: UserPhoneVerification):
+async def change_phone_number(db: db_depenency, user: user_dependency, phone_number: str):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed")
     user_model = db.query(Users).filter(Users.id == user.get("id")).first()
-    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Password")
-    user_model.phone_number = user_verification.new_phone_number
+    user_model.phone_number = phone_number
     db.add(user_model)
     db.commit()
