a64dec3ea31a7be6b489b36f7d9a4ddc9c683813
Luke McGuire <luke.mcguire@gmail.com>
2025-02-28 10:41:22 -0800

    config: change db from sqlite to postgresql

--- LM/project-3/TodoApp/database.py
+++ LM/project-3/TodoApp/database.py
@@ -1,10 +1,15 @@
+from os import getenv
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = getenv("POSTGRES_DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
